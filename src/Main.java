003fd2b1a5056f0a326f881c99484de33aaa769b
fer96carvalho <f.luiz1996@gmail.com>
2022-07-21 21:39:35 -0400

    Classe Mae Conteudo

--- src/Main.java
+++ src/Main.java
@@ -15,7 +15,10 @@ public class Main {
         curso2.setDescricao("Descrição curso javascript");
         curso2.setCargaHoraria(8);
 
-        Mentoria mentoria = new Mentoria("Mentoria Java", "Descrição mentoria Java", LocalDate.now());
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria java");
+        mentoria.setDescricao("Descrição mentoria java");
+        mentoria.setData(LocalDate.now());
 
         System.out.println(curso1);
         System.out.println(curso2);
